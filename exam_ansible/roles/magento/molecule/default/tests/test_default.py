35b7676905e6c2a42a3eb4348ae1d38cb4497d9d
Ubuntu <ubuntu@ip-172-31-7-127.eu-west-3.compute.internal>
2025-01-04 08:46:04 +0000

    ajout molecule

---
+++ exam_ansible/roles/magento/molecule/default/tests/test_default.py
@@ -0,0 +1,19 @@
+"""Role testing files using testinfra."""
+
+import requests
+
+def test_services_running(host):
+    nginx = host.service("nginx")
+    mysql = host.service("mysql")
+
+    assert nginx.is_running
+    assert mysql.is_running
+
+def test_magento_homepage(host):
+    response = requests.get("http://ec2-13-38-73-15.eu-west-3.compute.amazonaws.com")
+    assert response.status_code == 200, "La page d'accueil Magento ne répond pas correctement"
+
+
+def test_magento_db_connection(host):
+    command = host.run("mysql -u magento_user -ppassword -e 'SHOW DATABASES;'")
+    assert "magento_db" in command.stdout, "Magento n'a pas accès à la base de données"
